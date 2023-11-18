cf9bcc38af7fec0bee2d490e22943384eff63852
miladshakerdn <milad1shaker@gmail.com>
2023-11-18 15:08:43 +0330

    fix advcode

---
+++ plugins/advcode/langs/fa.js
@@ -0,0 +1,16 @@
+/*!
+ * TinyMCE Language Pack
+ *
+ * Copyright (c) 2023 Ephox Corporation DBA Tiny Technologies, Inc.
+ * Licensed under the Tiny commercial license. See https://www.tiny.cloud/legal/
+ */
+tinymce.addI18n("fa", {
+    "Save code": "\u0630\u062e\u06cc\u0631\u0647 \u06a9\u062f",
+    "Cancel": "\u0644\u063a\u0648",
+    "Fullscreen": "\u062a\u0645\u0627\u0645\u200c\u0635\u0641\u062d\u0647",
+    "Copy code": "\u06a9\u067e\u06cc \u06a9\u062f",
+    "Dark/light mode": "\u062d\u0627\u0644\u062a \u062a\u06cc\u0631\u0647/\u0631\u0648\u0634\u0646",
+    "Increase font size": "\u0627\u0641\u0632\u0627\u06cc\u0634 \u0627\u0646\u062f\u0627\u0632\u0647 \u0641\u0648\u0646\u062a",
+    "Decrease font size": "\u06a9\u0627\u0647\u0634 \u0627\u0646\u062f\u0627\u0632\u0647 \u0641\u0648\u0646\u062a"
+  });
+  
---
+++ plugins/mergetags/langs/fa.js
@@ -0,0 +1,13 @@
+/*!
+ * TinyMCE Language Pack
+ *
+ * Copyright (c) 2023 Ephox Corporation DBA Tiny Technologies, Inc.
+ * Licensed under the Tiny commercial license. See https://www.tiny.cloud/legal/
+ */
+tinymce.addI18n("fa", {
+    "Merge tag": "Merge tag",
+    "Insert merge tag": "\u062f\u0631\u062c merge tag",
+    "Filter merge tags": "\u0641\u06cc\u0644\u062a\u0631 \u06a9\u0631\u062f\u0646 merge tags",
+    "No matches": "\u0645\u0648\u0631\u062f \u0645\u0646\u0637\u0628\u0642\u06cc \u0648\u062c\u0648\u062f \u0646\u062f\u0627\u0631\u062f"
+  });
+  
